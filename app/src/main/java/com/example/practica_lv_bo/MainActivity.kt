722160d75f71aaf7d423b65683079233048a9ba0
Marshmottel <ob133@student.ugal.ro>
2023-06-29 09:05:04 +0300

    proiect

--- app/src/main/java/com/example/practica_lv_bo/MainActivity.kt
+++ app/src/main/java/com/example/practica_lv_bo/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.practica_lv_bo
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var imageView: ImageView
+    lateinit var button: Button
+
+    val cardImages = IntArray{
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
